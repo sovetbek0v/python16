aa971308d587a4d3b121b0fbced37ff400f6fffb
sovetbek0v <sovetbekovttemirlan@gmail.com>
2022-01-14 20:38:02 +0600

    day2

--- product/models.py
+++ product/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.db.models import Choices
 from model_utils.fields import StatusField
 
+from account.models import User
+
 
 class CreatedAtModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True,
@@ -28,6 +30,7 @@ class Product(CreatedAtModel):
 
 class ProductReview(CreatedAtModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews', null=True)
     text = models.TextField()
     rating = models.PositiveIntegerField(default=1)
 
--- shopApi/settings.py
+++ shopApi/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'product',
     'order',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'account.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
